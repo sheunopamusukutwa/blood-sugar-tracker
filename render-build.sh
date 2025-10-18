6a8176a665ff5d59e6b58bfc6279e54d6bb51ee3
sheunopamusukutwa <scmusukutwa@gmail.com>
2025-10-19 00:11:38 +0200

    Update render file

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # Exit on error
+
 set -o errexit
+
+# Install deps (Render won't do this if we override the build command)
+python -m pip install --upgrade pip
+pip install -r requirements.txt
 
 echo "🔧 Running database migrations..."
 python manage.py migrate --noinput
